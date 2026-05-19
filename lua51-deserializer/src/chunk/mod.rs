5ca1875bc5e991d6bf762d1753f21d761c53b702
july <219426848+Scrensayol@users.noreply.github.com>
2026-05-19 20:56:37 +0700

    Update mod.rs

--- lua51-deserializer/src/chunk/mod.rs
+++ lua51-deserializer/src/chunk/mod.rs
@@ -19,16 +19,13 @@ pub struct Chunk<'a> {
 impl<'a> Chunk<'a> {
     pub fn parse(input: &'a [u8]) -> IResult<&[u8], Self> {
         let (input, header) = Header::parse(input)?;
-        // TODO: pass header to Function::parse
+
         assert_eq!(header.version_number, 0x51);
         assert_eq!(header.format, Format::Official);
         assert_eq!(header.endianness, Endianness::Little);
-        assert_eq!(header.int_width as usize, mem::size_of::<i32>());
-        assert_eq!(header.size_t_width as usize, mem::size_of::<u32>());
-        assert_eq!(header.instr_width as usize, mem::size_of::<u32>());
-        assert_eq!(header.number_width as usize, mem::size_of::<f64>());
-        assert!(!header.number_is_integral);
-        let (input, function) = Function::parse(input)?;
+        assert!(header.size_t_width == 4 || header.size_t_width == 8);
+
+        let (input, function) = Function::parse(input, header.size_t_width)?;
 
         Ok((input, Self { function }))
     }
